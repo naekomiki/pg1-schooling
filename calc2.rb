ffb4c811306ece5b3bc601ab9e3b6adf342d4c86
三木　菜恵子 <bu2277350008@bukkyo-u.ac.jp>
2024-07-29 17:45:07 +0900

    Update calc2.rb

--- calc2.rb
+++ calc2.rb
@@ -2,3 +2,11 @@ numbers = [1, 2, 3, 4, 5]
 average = numbers.sum.to_f / numbers.size
 puts average
 
+
+numbers = [1, 2, 3, 4, 5,6]
+average = numbers.sum.to_f / numbers.size
+puts average
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+average = numbers.sum.to_f / numbers.size
+puts average
